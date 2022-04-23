d1637f8433b8422a234f8d5dc4b03565a8fbe36b
Ariadna S <iamariadnaayala@gmail.com>
2022-04-23 01:43:21 -0500

    Create PA4_source.py

---
+++ PA4/PA4_source.py
@@ -0,0 +1,17 @@
+# define following two functions
+def make_postage(money):
+
+
+def permutations(tuple_pairs):
+        
+## proided
+
+def permutation(s) :
+    from itertools import permutations
+    return set(permutations(s))
+
+
+if __name__=='__main__' :
+    amount = input()
+    print("change for ", amount, " : ", make_postage(int(amount)))
+    print("permutations:  ", permutations((1, 2)))
